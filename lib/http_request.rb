d0b5479a5dd743b169079ae88f8fdb4df69c9e0e
Mary <mary@LAPTOP-1P06IC7C.localdomain>
2025-10-04 15:42:25 +0800

    api_explore_hcg_3

--- lib/http_request.rb
+++ lib/http_request.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-def http_request(name, param)
+def http_request(name, param = nil)
   if name == 'hccg'
     response = HTTP.get("https://opendata.hccg.gov.tw/API/v3/Rest/OpenData/45A6214813A52DED?take=#{param['take']}&skip=#{param['skip']}")
-    raise 'Request Failed' unless response.status.to_s.to_i == 200
+  # insert other apis here
+  else
+    raise 'Unknown API'
   end
+  raise 'Request Failed' unless response.status.to_s.to_i == 200
+
   response
 end
